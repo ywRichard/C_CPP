85c20f353cd989df568a12d2bc0afe239d5a2dae
Mac VSCode <ywfire0620@163.com>
2018-09-02 11:58:17 +0800

    debug in VS

--- 01_CDemo/01_PointOnC/Exercise_6.c
+++ 01_CDemo/01_PointOnC/Exercise_6.c
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "stdafx.h"
+// #include "stdafx.h"
 
+#define LIST_LEN (unsigned int)100
 //Page 115, Point on C
 //1: return the first character point in source of which matches any characters in chars. Otherwise, return NULL.
 char *find_char(char const *source, char const *chars);
@@ -15,6 +16,9 @@ void reverse_string(char *string);
 void reverse_string01(char *string);
 //reverse by recursion
 void reverse_string02(char *string);
+
+//4. Find prime in a range
+char *find_prime(char *src, unsigned int len);
 
 int main()
 {
@@ -32,19 +36,19 @@ int main()
 	//     printf("No match character in source.\n");
 	// }
 
-	 //2.test sample for func del_substr()
-	char str[10] = "abcdefg";
-	char substr[10] = "ab";
-	printf("del_substr begin\n");
-	int result = del_substr(str, substr);
-	if (result != 0)
-	{
-		printf("str:%s\n", str);
-	}
-	else
-	{
-		printf("No matched in str\n");
-	}
+	//2.test sample for func del_substr()
+	// char str[10] = "abcdefg";
+	// char substr[10] = "ab";
+	// printf("del_substr begin\n");
+	// int result = del_substr(str, substr);
+	// if (result != 0)
+	// {
+	// 	printf("str:%s\n", str);
+	// }
+	// else
+	// {
+	// 	printf("No matched in str\n");
+	// }
 
 	// 3.test sample for func reverse_string()
 	//char str[10] = "abcef";
@@ -52,6 +56,19 @@ int main()
 	//reverse_string02(str);
 	//printf("reverse_string:%s\n", str);
 	//return 0;
+
+	// 4.Find out all the prime number from the range
+	char list[LIST_LEN] = {0};
+	find_prime(list, LIST_LEN);
+	printf("the prime:2 ");
+	for (int i = 1; i < LIST_LEN; i++)
+	{
+		if (*(list + i) == 'T')
+		{
+			printf("%d ", 2 * i + 1);
+		}
+	}
+	printf("\n");
 }
 
 char *find_char(char const *source, char const *chars)
@@ -112,7 +129,8 @@ int del_substr(char *str, char const *substr)
 	return 0;
 }
 
-void reverse_string(char *str) {
+void reverse_string(char *str)
+{
 	char *last_char;
 	for (last_char = str; *last_char != '\0'; last_char++)
 		;
@@ -153,7 +171,8 @@ void reverse_string01(char *string)
 }
 
 //reverse by recursion
-void reverse_string02(char *string) {
+void reverse_string02(char *string)
+{
 	if (string == NULL)
 	{
 		printf("func paramter error");
@@ -165,4 +184,40 @@ void reverse_string02(char *string) {
 
 	reverse_string02(++string);
 	printf("%c", *string);
+}
+
+char *find_prime(char *src, unsigned int len)
+{
+	if (len == 0)
+		return NULL;
+	for (int i = 0; i < len; i++)
+	{
+		*(src + i) = 'T';
+	}
+
+	for (int i = 1; i < len; i++)
+	{
+		if (*(src + i) == 'F')
+			continue;
+
+		int prime = 2 * i + 1;
+
+		for (int j = i; j < len; j++)
+		{
+			if (*(src + j) == 'F')
+				continue;
+
+			int num = 2 * j + 1;
+			// *(src + 1) = (num % prime) == 0 ? 'F' : 'T';
+			if (num % prime == 0)
+			{
+				if (num == prime)
+					*(src + 1) = 'T';
+				else
+					*(src + 1) = 'F';
+			}
+		}
+	}
+
+	return src;
 }
